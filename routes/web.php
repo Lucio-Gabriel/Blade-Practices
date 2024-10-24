665464b3e0cf19c2c7f84f89d641b7878b4c5cb9
Gabriel <suppershock777@gmail.com>
2024-10-24 11:11:54 -0300

    Create routes of livewire and blade

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/livewire', [HomeController::class, 'livewire'])->name('index.livewire');
+
+Route::get('/blade', [HomeController::class, 'blade'])->name('index.blade');

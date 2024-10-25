3d5a91baae3cde7d5d32bb1819ed7f2464179b1a
Gabriel <suppershock777@gmail.com>
2024-10-25 17:44:16 -0300

    created our employee creator in livewire

--- app/Livewire/CreateEmployees.php
+++ app/Livewire/CreateEmployees.php
@@ -2,10 +2,37 @@
 
 namespace App\Livewire;
 
+use App\Models\Employee;
 use Livewire\Component;
 
 class CreateEmployees extends Component
 {
+
+    public string $name = '';
+    public string $email = '';
+    public string $endereco = '';
+    public string $genero = '';
+    public string $cargo = '';
+    public string $telefone = '';
+    public string $cpf = '';
+
+    public function save()
+    {
+        Employee::create(
+            $this->only([
+                'name',
+                'email',
+                'endereco',
+                'genero',
+                'cargo',
+                'telefone',
+                'cpf'
+            ])
+        );
+
+        return $this->redirect(route('index.livewire'));
+    }
+
     public function render()
     {
         return view('livewire.create-employees');

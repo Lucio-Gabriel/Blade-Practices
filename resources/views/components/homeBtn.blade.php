8c6a23af98a6a01ddeb40e0d9fc0b4c5b802add4
Gabriel <suppershock777@gmail.com>
2024-10-25 09:44:59 -0300

    created our back button to our home page

---
+++ resources/views/components/homeBtn.blade.php
@@ -0,0 +1,6 @@
+<a
+class="text-white bg-red-500 p-5 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring focus:ring-red-500"
+href="{{ route('index') }}
+">
+Voltar para a pagina inicial
+</a>

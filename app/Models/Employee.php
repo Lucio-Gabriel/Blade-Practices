60add9778d7f11bd57540307219df71a2fce4351
Gabriel <suppershock777@gmail.com>
2024-10-25 10:10:32 -0300

    created our employee model

---
+++ app/Models/Employee.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use function PHPSTORM_META\map;
+
+class Employee extends Model
+{
+    //
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'name',
+        'cpf',
+        'idade',
+        'genero',
+        'email',
+        'email',
+        'telefone',
+        'endereco',
+        'data_contratacao',
+        'salario',
+        'cargo',
+
+    ];
+}

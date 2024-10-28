2951042fb0201f08470a01a5b4d7ad66d2912b01
Gabriel <suppershock777@gmail.com>
2024-10-28 11:02:56 -0300

    created our livewire component and rule

--- app/Livewire/AdminLogin.php
+++ app/Livewire/AdminLogin.php
@@ -2,10 +2,23 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 
 class AdminLogin extends Component
 {
+
+    public string $email = '';
+
+    public string $password = '';
+
+    public function save()
+    {
+        User::create(['name', 'password']);
+
+       return $this->redirect('/escolha-aqui');
+    }
+
     public function render()
     {
         return view('livewire.admin-login');
